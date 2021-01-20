571e92a21500ed5e2f8531c59e879c56e3413300
marylotta <marymaximava@gmail.com>
2021-01-20 11:47:46 +0300

    complete day1 tasks

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -1,7 +1,19 @@
 package day1;
+/*
+ Вывести на экран слово “JAVA”, в строку, чтобы оно повторилось 10 раз, используя цикл while.
+Вывод в консоль должен быть таким:
+JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA JAVA
+ */
 
 public class Task1 {
     public static void main(String[] args) {
+
+        int x = 0;
+        while (x<10){
+            System.out.print("JAVA ");
+            x++;
+        }
+
 
     }
 }
--- src/main/java/day1/Task2.java
+++ src/main/java/day1/Task2.java
@@ -1,7 +1,14 @@
 package day1;
+/*
+2. Повторить задание 1, но используя цикл for.
+ */
 
 public class Task2 {
     public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.print("JAVA ");
+
+        }
 
     }
 }
