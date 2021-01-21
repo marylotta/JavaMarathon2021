b5357f14a33cd522d82d5595c29497102c59b390
marylotta <marymaximava@gmail.com>
2021-01-21 12:22:00 +0300

    Complete day2 tasks

--- src/main/java/day2/Task2.java
+++ src/main/java/day2/Task2.java
@@ -1,7 +1,22 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
 
+        if (a < b) {
+
+            for (a++; a < b; a++) {
+                if (a % 5 == 0 && a % 10 != 0) {
+                    System.out.print(a + " ");
+                }
+            }
+        } else if (a >= b) {
+            System.out.println("Некорректный ввод");
+        }
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,26 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        if (a >= b) {
+            System.out.println("Некорректный ввод");
+        }
+        while (a < b) {
+            a++;
+            if (a % 5 == 0 && a % 10 != 0) {
+                System.out.print(a + " ");
+            }
+            
+            if (a == b) {
+                break;
+            }
+        }
 
     }
 }
