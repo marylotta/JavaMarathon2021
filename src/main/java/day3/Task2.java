095e26073a07bf72478bab37e5c1968934b51f47
marylotta <marymaximava@gmail.com>
2021-01-22 13:34:09 +0300

    Complete day3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double number1;
+        double number2;
 
+        while (true) {
+            number1 = scanner.nextDouble();
+            number2 = scanner.nextDouble();
+            if (number2 == 0) {
+                break;
+            }
+            System.out.println(number1 / number2);
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        for (int i = 0; i < 5; i++) {
+
+            double number1 = scanner.nextDouble();
+            double number2 = scanner.nextDouble();
+
+            if (number2 == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(number1 / number2);
+        }
     }
 }
