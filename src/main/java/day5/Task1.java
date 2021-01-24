25e2b8aa0244185bcc3862bd544802bc30d0def8
marylotta <marymaximava@gmail.com>
2021-01-24 15:31:31 +0300

    Complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,14 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setYear(2020);
+        car1.setColor("White");
+        car1.setModel("Honda");
+
+        System.out.println("Год выпуска: " + car1.getYear());
+        System.out.println("Цвет: " + car1.getColor());
+        System.out.println("Модель: " + car1.getModel());
 
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,10 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike moto = new Motorbike(2019, "Black", "Harley Davidson");
 
+        System.out.println("Год выпуска: " + moto.getYear());
+        System.out.println("Цвет: " + moto.getColor());
+        System.out.println("Модель: " + moto.getModel());
     }
 }
