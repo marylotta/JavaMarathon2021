7439a46e019397000f5a26cdca701dead0c95a8e
marylotta <marymaximava@gmail.com>
2021-01-27 18:16:34 +0300

    Complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,27 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            builder.append(i + " ");
+        }
+        long end = System.currentTimeMillis();
+
+        System.out.println("Time StringBuilder " + (end - start));
+        System.out.println(builder.toString());
+
+        String s = "";
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 20000; i++) {
+            s += i + " ";
+        }
+        end = System.currentTimeMillis();
+        System.out.println("Time String " + (end - start));
+        System.out.println(s);
 
     }
+
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,8 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplane airplane = new Airplane("Airbus", 1998, 15000, 30000);
+        System.out.println(airplane);
 
     }
 }
